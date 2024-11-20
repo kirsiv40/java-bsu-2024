7534141061c03af9fe411579818b8d93374b32ee
kirsiv40 <ttvgch@gmail.com>
2024-11-20 19:50:25 +0300

    small refactoring

--- lab-2/src/main/java/by/bsu/dependency/context/SimpleApplicationContext.java
+++ lab-2/src/main/java/by/bsu/dependency/context/SimpleApplicationContext.java
@@ -24,43 +24,41 @@ public class SimpleApplicationContext extends AbstractApplicationContext {
     protected Map<Class<?>, DefnRecord<?>> beanDefinitionsByClass = new HashMap<>();
     protected Map<String, Object> singletonBeans = new HashMap<>();
     protected Map<Class<?>, Object> singletonBeansByClass = new HashMap<>();
-    
-    protected record recClassStorage<T>(DefnRecord<T> definition, T instance) {
-    }
 
     public SimpleApplicationContext(Collection<Class<?>> beanClassesCollection, boolean takeClassesWithoutAnnotation) {
         super(new HashMap<String, DefnRecord<?>>(beanClassesCollection.stream()
-            .filter(el -> {
-                if (takeClassesWithoutAnnotation) {
-                    return true;
-                }
-                return el.isAnnotationPresent(Bean.class);
-            })
-            .collect(Collectors.toMap(el -> {
-                if (el.isAnnotationPresent(Bean.class) && !el.getAnnotation(Bean.class).name().isEmpty()) {
-                    return el.getAnnotation(Bean.class).name();
-                } else {
-                    String[] allnames = el.getName().split("\\.");
-                    String realname = allnames[allnames.length - 1];
-                    String temp = Character.toLowerCase(realname.charAt(0)) + realname.substring(1);
-                    return temp;
-                }
-            },
-            el -> {
-                Optional<Method> method = Arrays.asList(el.getDeclaredMethods()).stream()
-                    .filter(el_method -> el_method.isAnnotationPresent(PostConstruct.class))
-                    .findAny();
-                if (el.isAnnotationPresent(Bean.class)) {
-                    if (el.getAnnotation(Bean.class).name().isEmpty()) {
-                        return new DefnRecord<>(el, el.getAnnotation(Bean.class).scope(), Character.toLowerCase(el.getName().charAt(0)) + el.getName().substring(1), method);
-                    } else {
-                        return new DefnRecord<>(el, el.getAnnotation(Bean.class).scope(), el.getAnnotation(Bean.class).name(), method);
+                .filter(el -> {
+                    if (takeClassesWithoutAnnotation) {
+                        return true;
                     }
-                } else {
-                    return new DefnRecord<>(el, BeanScope.SINGLETON, Character.toLowerCase(el.getName().charAt(0)) + el.getName().substring(1), method);
-                }
-            }
-        ))));
+                    return el.isAnnotationPresent(Bean.class);
+                })
+                .collect(Collectors.toMap(el -> {
+                    if (el.isAnnotationPresent(Bean.class) && !el.getAnnotation(Bean.class).name().isEmpty()) {
+                        return el.getAnnotation(Bean.class).name();
+                    } else {
+                        return GenerateBeanName(el);
+                    }
+                },
+                el -> {
+                    Optional<Method> method = Arrays.asList(el.getDeclaredMethods()).stream()
+                            .filter(el_method -> el_method.isAnnotationPresent(PostConstruct.class))
+                            .findAny();
+                    if (el.isAnnotationPresent(Bean.class)) {
+                        if (el.getAnnotation(Bean.class).name().isEmpty()) {
+                            return new DefnRecord<>(el, el.getAnnotation(Bean.class).scope(),
+                                    Character.toLowerCase(el.getName().charAt(0)) + el.getName().substring(1),
+                                    method);
+                        } else {
+                            return new DefnRecord<>(el, el.getAnnotation(Bean.class).scope(),
+                                    el.getAnnotation(Bean.class).name(), method);
+                        }
+                    } else {
+                        return new DefnRecord<>(el, BeanScope.SINGLETON,
+                                Character.toLowerCase(el.getName().charAt(0)) + el.getName().substring(1),
+                                method);
+                    }
+                }))));
         for (var el : beanDefinitions.values()) {
             beanDefinitionsByClass.put(el.clazz(), el);
         }
@@ -69,10 +67,12 @@ public class SimpleApplicationContext extends AbstractApplicationContext {
     /**
      * Создает контекст, содержащий классы, переданные в параметре.
      * <br/>
-     * Если на классе нет аннотации {@code @Bean}, имя бина получается из названия класса, скоуп бина по дефолту
+     * Если на классе нет аннотации {@code @Bean}, имя бина получается из названия
+     * класса, скоуп бина по дефолту
      * считается {@code Singleton}.
      * <br/>
-     * Подразумевается, что у всех классов, переданных в списке, есть конструктор без аргументов.
+     * Подразумевается, что у всех классов, переданных в списке, есть конструктор
+     * без аргументов.
      *
      * @param beanClasses классы, из которых требуется создать бины
      */
@@ -84,17 +84,66 @@ public class SimpleApplicationContext extends AbstractApplicationContext {
         this(new ArrayList<Class<?>>(Arrays.asList(beanClasses)), takeWA);
     }
 
+    protected void InjectStaticBean(Field field, String name, Map<Class<?>, Object> rec_singletons) {
+        if (!beanDefinitionsByClass.containsKey(field.getType())) {
+            throw new NoSuchBeanFoundToInjectException(field.getName());
+        }
+        field.setAccessible(true);
+        try {
+            field.set(singletonBeans.get(name), getBean(field.getType(), new HashMap<>(), rec_singletons));
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    protected void InjectBean(Field field, Object instance, Class<?> clazz, Map<Class<?>, Object> rec_singletons,
+            Map<Class<?>, Object> new_rec_deps) {
+        field.setAccessible(true);
+        try {
+            if (field.getAnnotation(Inject.class).canUseLastSameClass() && new_rec_deps.containsKey(field.getType())) {
+                field.set(instance, new_rec_deps.get(field.getType()));
+            } else if (beanDefinitionsByClass.get(field.getType()).scope().equals(BeanScope.BLOCK_SINGLETON)) {
+                if (!rec_singletons.containsKey(clazz)) {
+                    rec_singletons.put(clazz, getBean(field.getType(), new_rec_deps, rec_singletons));
+                }
+                field.set(instance, rec_singletons.get(clazz));
+            } else {
+                field.set(instance, getBean(field.getType(), new_rec_deps, rec_singletons));
+            }
+        } catch (NoSuchBeanDefinitionException e) {
+            throw e;
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected <T> void CallPostConstruct(DefnRecord<T> record, Object instance) {
+        if (record.init().isPresent()) {
+            try {
+                Method method = record.init().get();
+                method.setAccessible(true);
+                method.invoke(instance);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+    }
+
     /**
-     * Помимо прочего, метод должен заниматься внедрением зависимостей в создаваемые объекты
+     * Помимо прочего, метод должен заниматься внедрением зависимостей в создаваемые
+     * объекты
      */
     @Override
     public void start() {
+        // обход и вызов конструктора всех минглтон бинов
         for (String name : beanDefinitions.keySet()) {
             if (beanDefinitions.get(name).scope().equals(BeanScope.SINGLETON)) {
                 try {
                     singletonBeans.put(name, beanDefinitions.get(name).clazz()
-                    .getConstructor()
-                    .newInstance());
+                            .getConstructor()
+                            .newInstance());
                 } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
                         | InvocationTargetException | NoSuchMethodException | SecurityException e) {
                     e.printStackTrace();
@@ -103,51 +152,25 @@ public class SimpleApplicationContext extends AbstractApplicationContext {
             }
         }
 
+        // создание мапы для поиска по классу
         for (var el : singletonBeans.values()) {
             singletonBeansByClass.put(el.getClass(), el);
         }
 
         for (String name : singletonBeans.keySet()) {
-            // fields init
+            // заполнение полей
             Map<Class<?>, Object> rec_singletons = new HashMap<>();
             for (Field field : singletonBeans.get(name).getClass().getDeclaredFields()) {
                 if (field.isAnnotationPresent(Inject.class)) {
-                    if (!beanDefinitionsByClass.containsKey(field.getType())) {
-                        throw new NoSuchBeanFoundToInjectException(field.getName());
-                    }
-                    field.setAccessible(true);
-                    try {
-                        field.set(singletonBeans.get(name), getBean(field.getType(), new HashMap<>(), rec_singletons));
-                    } catch (IllegalArgumentException | IllegalAccessException e) {
-                        e.printStackTrace();
-                        return;
-                    }
+                    InjectStaticBean(field, name, rec_singletons);
                 }
             }
-            // post construct call
+            // пост конструкт
             if (beanDefinitions.get(name).init().isPresent()) {
-                try {
-                    Method method = beanDefinitions.get(name).init().get();
-                    method.setAccessible(true);
-                    method.invoke(singletonBeans.get(name));
-                } catch (IllegalAccessException | InvocationTargetException e) {
-                    e.printStackTrace();
-                    return;
-                }
+                CallPostConstruct(beanDefinitions.get(name), singletonBeans.get(name));
             }
         }
         this.is_started = ContextStatus.STARTED;
-    }
-
-    @Override
-    public boolean containsBean(String name) {
-        if (!isRunning()) {
-            throw new ApplicationContextNotStartedException();
-        }
-        if (beanDefinitions.containsKey(name)) {
-            return true;
-        }
-        return false;
     }
 
     @Override
@@ -169,8 +192,13 @@ public class SimpleApplicationContext extends AbstractApplicationContext {
         return getBean(clazz, new HashMap<>(), new HashMap<>());
     }
 
+    /**
+     * 
+     * @param rec_deps для поиска и исправления зависимостей с BLOCK_ELEMENT
+     * @param rec_singletons для поиска и исправления зависимостей с BLOCK_SINGLETON
+     */
     @SuppressWarnings("unchecked")
-    protected <T> T getBean(Class<T> clazz, Map<Class<?>, recClassStorage<?>> rec_deps, Map<Class<?>, Object> rec_singletons) {
+    protected <T> T getBean(Class<T> clazz, Map<Class<?>, Object> rec_deps, Map<Class<?>, Object> rec_singletons) {
         if (!beanDefinitionsByClass.containsKey(clazz)) {
             throw new NoSuchBeanDefinitionException();
         }
@@ -180,14 +208,14 @@ public class SimpleApplicationContext extends AbstractApplicationContext {
         }
 
         if (rec_deps.containsKey(clazz) && record.scope().equals(BeanScope.BLOCK_ELEMENT)) {
-            return (T) rec_deps.get(clazz).instance;
+            return (T) rec_deps.get(clazz);
         }
 
         if (rec_deps.containsKey(clazz)) {
             throw new CircularDependencyFoundException(clazz.getName());
         }
 
-        Map<Class<?>, recClassStorage<?>> new_rec_deps = new HashMap<>(rec_deps);
+        Map<Class<?>, Object> new_rec_deps = new HashMap<>(rec_deps);
 
         Object newInstance = null;
         try {
@@ -198,42 +226,17 @@ public class SimpleApplicationContext extends AbstractApplicationContext {
             return null;
         }
 
-        new_rec_deps.put(clazz, new recClassStorage<T>(record, (T) newInstance));
+        new_rec_deps.put(clazz, newInstance);
 
-        //fields init
+        // Заполнение полей
         for (Field field : newInstance.getClass().getDeclaredFields()) {
             if (field.isAnnotationPresent(Inject.class)) {
-                field.setAccessible(true);
-                try {
-                    if (field.getAnnotation(Inject.class).canUseLastSameClass() && new_rec_deps.containsKey(field.getType())) {
-                        field.set(newInstance, new_rec_deps.get(field.getType()).instance);
-                    } else if (beanDefinitionsByClass.get(field.getType()).scope().equals(BeanScope.BLOCK_SINGLETON)) { 
-                        if (!rec_singletons.containsKey(clazz)) {
-                            rec_singletons.put(clazz, getBean(field.getType(), new_rec_deps, rec_singletons));
-                        }
-                        field.set(newInstance, rec_singletons.get(clazz));
-                    } else {
-                        field.set(newInstance, getBean(field.getType(), new_rec_deps, rec_singletons));
-                    }
-                } catch(NoSuchBeanDefinitionException e) {
-                    throw e;
-                } catch (IllegalArgumentException | IllegalAccessException e) {
-                    e.printStackTrace();
-                }
+                InjectBean(field, newInstance, clazz, rec_singletons, new_rec_deps);
             }
         }
 
-        //PostConstruct
-        if (record.init().isPresent()) {
-            try {
-                Method method = record.init().get();
-                method.setAccessible(true);
-                method.invoke(newInstance);
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
+        // пост конструкт
+        CallPostConstruct(record, newInstance);
         return (T) newInstance;
     }
 }
--- lab-2/src/main/java/by/bsu/example/ThirdBean.java
+++ lab-2/src/main/java/by/bsu/example/ThirdBean.java
@@ -3,10 +3,11 @@ package by.bsu.example;
 import by.bsu.dependency.annotation.Bean;
 import by.bsu.dependency.annotation.BeanScope;
 import by.bsu.dependency.annotation.Inject;
+import by.bsu.dependency.annotation.PostConstruct;
 
 @Bean(scope = BeanScope.PROTOTYPE)
 public class ThirdBean {
-    public static String name = "C";
+    public String name = "C";
     public int counter = 0;
 
     @Inject(canUseLastSameClass = true)
@@ -20,5 +21,10 @@ public class ThirdBean {
         } else {
             System.out.println("Got back to the first bean \"" + name + "\". Stopping.");
         }
+    }
+
+    @PostConstruct
+    void init() {
+        name = "C";
     }
 }
